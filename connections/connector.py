351e8fc7efaadaab42fd6124746781c574bb916d
chukhranchick <ivan.chuhran@gmail.com>
2022-12-24 18:25:16 +0200

    proceed execution w/out return value

--- connections/connector.py
+++ connections/connector.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 
 import oracledb
 
+
 class Connector:
     def __init__(self, user: str = None, password: str = None, host: str = None, port: int = None, dsn: str = None):
         self.__pool = oracledb.create_pool(user=user, password=password, host=host, port=port, dsn=dsn)
@@ -13,7 +14,14 @@ class Connector:
         with self.connect() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(query, *args, **kwargs)
+                conn.commit()
                 return cursor.fetchall()
+
+    def execute_without_return(self, query: str, *args, **kwargs):
+        with self.connect() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, *args, **kwargs)
+                conn.commit()
 
     def close(self):
         self.__pool.close()
